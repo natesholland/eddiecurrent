d038fc6985a1a5362c68ea92226af51152f0a85c
natesholland <natesholland@gmail.com>
2013-12-23 21:08:49 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eddiecurrent::Application.config.secret_token = '13da0c8c6c699dbb64c33ecf0d3095e4c20276100c7cb82137b5e345ce937cc46de65d10c5a6c139faea9afcf1604e3d6020199c352c6472f500e2bf0b27598a'
